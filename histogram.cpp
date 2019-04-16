a9b02c16912ca1fab52e4e7ec6a06b2d8db0ce98
TitovaVD <lera.titova.2000@list.ru>
2019-04-16 03:31:39 +0300

    code:функция поиска минимума и максимума занесена в файл реализации

---
+++ histogram.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include "histogram.h"
+using namespace std;
+void
+find_minmax(vector<double> numbers, double& min, double& max)
+{
+    if(numbers.size()== 0);
+    else{
+    min = numbers[0];
+    max = numbers[0];
+    for(double number : numbers)
+    {
+        if(number<min)
+            min=number;
+        if(number>max)
+            max=number;
+    }
+    }
+}
